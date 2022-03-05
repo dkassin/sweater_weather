121875d53e3e775e507b49fe02856f3e2cebd4fe
David Kassin <david.j.kassin@gmail.com>
2022-03-04 17:27:50 -0700

    spec for forest_service

---
+++ spec/services/forecast_service_spec.rb
@@ -0,0 +1,20 @@
+
+require 'rails_helper'
+
+RSpec.describe 'Forecast Service' do
+  it 'returns a faraday response' do
+      connection = ForecastService.connection
+      expect(connection).to be_a(Faraday::Connection)
+  end
+
+  it 'returns a recipe at random' do
+      location = "Denver,CO"
+      get_location = ForecastService.get_location(location)
+
+      expect(get_location).to be_a Hash
+      expect(get_location[:results]).to be_a Array
+      expect(get_location[:results][0][:locations][0][:latLng]).to be_a Hash
+      expect(get_location[:results][0][:locations][0][:latLng]).to have_key :lat
+      expect(get_location[:results][0][:locations][0][:latLng]).to have_key :lng
+  end
+end
