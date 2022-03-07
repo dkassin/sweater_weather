0a7619ae2983923d0dd3094bbad13f9fc8a9bbc1
David Kassin <david.j.kassin@gmail.com>
2022-03-07 11:00:23 -0700

    Add more robust testing for happy path and add edge case if quantity is negative

--- spec/requests/api/v1/book_request_spec.rb
+++ spec/requests/api/v1/book_request_spec.rb
@@ -6,9 +6,43 @@ RSpec.describe 'book search API' do
 
 
     book_search = JSON.parse(response.body, symbolize_names: true)
-    
+
     expect(response).to be_successful
     expect(response.status).to eq(200)
     expect(book_search).to be_a Hash
+    expect(book_search[:data]).to have_key(:attributes)
+    expect(book_search[:data][:attributes]).to have_key(:destination)
+    expect(book_search[:data][:attributes]).to have_key(:forecast)
+    expect(book_search[:data][:attributes][:forecast]).to have_key(:summary)
+    expect(book_search[:data][:attributes][:forecast]).to have_key(:temperature)
+    expect(book_search[:data][:attributes]).to have_key(:total_books_found)
+    expect(book_search[:data][:attributes]).to have_key(:books)
+    expect(book_search[:data][:attributes][:books].count).to eq(5)
+    expect(book_search[:data][:attributes][:books][0]).to have_key(:isbn)
+    expect(book_search[:data][:attributes][:books][0]).to have_key(:title)
+    expect(book_search[:data][:attributes][:books][0]).to have_key(:publisher)
+  end
+
+  it 'returns a book search even with a negative quantity', :vcr do
+
+    get "/api/v1/book-search?location=denver,co&quantity=-5"
+
+
+    book_search = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+    expect(book_search).to be_a Hash
+    expect(book_search[:data]).to have_key(:attributes)
+    expect(book_search[:data][:attributes]).to have_key(:destination)
+    expect(book_search[:data][:attributes]).to have_key(:forecast)
+    expect(book_search[:data][:attributes][:forecast]).to have_key(:summary)
+    expect(book_search[:data][:attributes][:forecast]).to have_key(:temperature)
+    expect(book_search[:data][:attributes]).to have_key(:total_books_found)
+    expect(book_search[:data][:attributes]).to have_key(:books)
+    expect(book_search[:data][:attributes][:books].count).to eq(5)
+    expect(book_search[:data][:attributes][:books][0]).to have_key(:isbn)
+    expect(book_search[:data][:attributes][:books][0]).to have_key(:title)
+    expect(book_search[:data][:attributes][:books][0]).to have_key(:publisher)
   end
 end
