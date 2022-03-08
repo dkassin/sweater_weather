328c09c6d9b7a8fa178373ab049579ddba11a448
David Kassin <david.j.kassin@gmail.com>
2022-03-08 12:32:39 -0700

    Add more robust sad path testing for user login

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,7 @@
 class Api::V1::UsersController < ApplicationController
-  before_action :check_all_fields
+  before_action :check_all_fields, :check_if_arguments_match, :check_if_email_unique
 
   def create
-      User.arguments_dont_match(params[:password], params[:password_confirmation])
-      User.email_not_unique(params[:email])
       new_user = User.create(user_params)
       render json: UserSerializer.user(new_user), status: 201
   end
@@ -17,5 +15,17 @@ private
   def check_all_fields
     if !params[:email].present? || !params[:password].present? || !params[:password_confirmation].present?
       render json: JSON.generate({error: 'missing fields'}), status: 400
+    end
+  end
+
+  def check_if_arguments_match
+    if params[:password] != params[:password_confirmation]
+      render json: JSON.generate({error: 'password dont match'}), status: 400
+    end
+  end
+
+  def check_if_email_unique
+    if User.find_by(email: params[:email])
+      render json: JSON.generate({error: 'email has already been taken'}), status: 400
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,15 +5,4 @@ class User < ApplicationRecord
             presence: {message: "can't be blank"},
             uniqueness: true
 
-  def self.arguments_dont_match(arg_1, arg_2)
-    if arg_1 != arg_2
-      render json: JSON.generate({error: 'password dont match'}), status: 400
-    end
-  end
-
-  def self.email_not_unique(arg_1)
-    if User.find_by(email: arg_1)
-      render json: JSON.generate({error: 'email has already been taken'}), status: 400
-    end
-  end
 end
